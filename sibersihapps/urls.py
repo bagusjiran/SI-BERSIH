5eaddaf9e363fbde19a0e4fff60d687813df1db5
firlyfahrizaa <firlyfahriza@yahoo.com>
2025-10-10 20:56:28 +0700

    menghapus register sementara

--- sibersihapps/urls.py
+++ sibersihapps/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.landingpage, name='index'),
     path('adminpage/', views.adminpage, name='adminpage'),
-    path('registerpage/', views.registerpage, name='registerpage'),
 ]
